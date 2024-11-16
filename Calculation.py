6c2541fa37026f227a2abc6613d329048b16af43
Thrix <133132579+Thrixes@users.noreply.github.com>
2024-11-16 23:20:20 +0300

    Update Calculation.py

--- Calculation.py
+++ Calculation.py
@@ -1,3 +1,18 @@
+#1kwh to usd : 0.17$
+country_avg_kWh = 0.17
+
+
+
 #1 Calculate Monthly Electricty Consumption
 #cost per KWh varries from country to country. 
-Electricity_Consumption_(kWh) =
+kWh_consumption = monthly_bill/country_avg_kWh
+  print(kWh_consumption)
+
+#2 Daily average consumption
+daily_kWh_consumption = kWh_consumption / 30 
+  #result is daily avg
+  print(daily_kWh_consumption)
+
+#3 Solar energy requirement 
+#peak sun hours differs from country to country 
+solar_daily_requirement = daily_kWh_consumption/ peak_sun_hours
